5341078a890206bbdcf2abc2eef569319e0245c9
Nath <55251304+candiath@users.noreply.github.com>
2025-11-29 19:13:05 -0300

    Add loading screen and NoScript warning; update HTML title

--- src/main.tsx
+++ src/main.tsx
@@ -3,8 +3,16 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+// Ocultar spinner cuando React se monta
+const loadingScreen = document.getElementById('loading-screen');
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>,
 )
+
+// Remover spinner después de que React renderice
+if (loadingScreen) {
+  loadingScreen.style.display = 'none';
+}
