37e4f1437156010a740cef90ba84509c8571f176
Nath <55251304+candiath@users.noreply.github.com>
2025-12-04 20:46:27 -0300

    Added silly little easter egg

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
       <div className="max-w-6xl mx-auto">
         <div className="flex flex-col sm:flex-row justify-between items-center gap-4">
           <p className="text-muted-foreground text-sm">© {new Date().getFullYear()} FullStack Developer. Todos los derechos reservados.</p>
-          <p>Creado con <span className="text-accent">♥</span> por {" "}
+          <p className="">Creado con <span className="text-accent icon-easter-egg cursor-pointer inline-block w-2.5"></span> por {" "}
             <span className="text-foreground font-semibold">Natán Candia</span>
           </p>
         </div>
