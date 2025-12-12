d64e60105fe07eb048fec6eb388d40a5c93ceb56
Nath <55251304+candiath@users.noreply.github.com>
2025-12-12 16:38:08 -0300

    HOTFIX: devtalles requires cookies in order to serve certificate image

--- src/data/courses.ts
+++ src/data/courses.ts
@@ -4,14 +4,14 @@ export const courses = [
     title: "React: de cero a experto - Edición 2025",
     organization: "DevTalles",
     url: "https://cursos.devtalles.com/certificates/o2rkihjv34",
-    image: "https://cdn.filestackcontent.com/j8WKMskIScC97dllh4na?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjQ3LCJwYXRoIjoiLyJ9&signature=601ec8b9160f658044a792f0e0d8d557ce98a161785af49ea44f8496170d68de",
+    // image: "https://cdn.filestackcontent.com/j8WKMskIScC97dllh4na",
   },
   {
     year: "2024",
     title: "JavaScript Moderno: Guía para dominar el lenguaje",
     organization: "DevTalles",
     url: "https://cursos.devtalles.com/certificates/1zzlixb5yx",
-    image: "https://cdn.filestackcontent.com/FcO6TDKSrWvlqRUVQLA9?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjQ4LCJwYXRoIjoiLyJ9&signature=79f59305564834762a862ff77f81a761ac67d44bd8a9c0b59ae46c3fa74fa397",
+    // image: "https://cdn.filestackcontent.com/FcO6TDKSrWvlqRUVQLA9?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjQ4LCJwYXRoIjoiLyJ9&signature=79f59305564834762a862ff77f81a761ac67d44bd8a9c0b59ae46c3fa74fa397",
   },
   {
     year: "2024",
@@ -26,13 +26,13 @@ export const courses = [
     title: "GIT+GitHub: Todo un sistema de control de versiones de cero",
     organization: "DevTalles",
     url: "https://cursos.devtalles.com/certificates/9cdssbyj8y",
-    image: "https://cdn.filestackcontent.com/eHTC5IiSIyP5XYalxd5F?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjQ4LCJwYXRoIjoiLyJ9&signature=79f59305564834762a862ff77f81a761ac67d44bd8a9c0b59ae46c3fa74fa397",
+    // image: "https://cdn.filestackcontent.com/eHTC5IiSIyP5XYalxd5F?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjQ4LCJwYXRoIjoiLyJ9&signature=79f59305564834762a862ff77f81a761ac67d44bd8a9c0b59ae46c3fa74fa397",
   },
   {
     year: "2024",
     title: "Visual Studio Code: Mejora tu velocidad para codificar",
     organization: "DevTalles",
     url: "https://cursos.devtalles.com/certificates/pmxdxglzd0",
-    image: "https://cdn.filestackcontent.com/01EydYCaQ9SKXwkQa5Ss?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjUwLCJwYXRoIjoiLyJ9&signature=3696e01e8463097c67d0458ce469438b8964f464e7f8976a31222e9651c3a9ca",
+    // image: "https://cdn.filestackcontent.com/01EydYCaQ9SKXwkQa5Ss?policy=eyJjYWxsIjpbInJlYWQiXSwiZXhwaXJ5IjoxNzY1NTY2NjUwLCJwYXRoIjoiLyJ9&signature=3696e01e8463097c67d0458ce469438b8964f464e7f8976a31222e9651c3a9ca",
   },
 ];
